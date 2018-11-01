6ff87903bddc2fcbe840aac1d5278fb30e111cd6
Kevin McCartney <hello@kevinmccartney.is>
2018-11-01 18:56:20 -0400

    animation examples

--- week-5/animations.js
+++ week-5/animations.js
@@ -1,5 +1,44 @@
 $(document).ready(function () {
-  $('#fadeIn')[0].on('click', function() {
-    this.fadeIn();
+  const fadeInBtn = $('#fadeIn');
+  console.log(fadeInBtn);
+
+  $('#fadeIn').on('click', function() {
+    $('.card').fadeIn();
+  });
+
+  $('#fadeOut').on('click', function() {
+    $('.card').fadeOut();
+  });
+
+  $('#slideUp').on('click', function() {
+    $('.card').slideUp();
+  })
+
+  $('#slideDown').on('click', function() {
+    $('.card').slideDown();
+  });
+
+  $('#animate').on('click', function() {
+    $('.card').animate(
+      { opacity: 0.25 },
+      300,
+      function() {
+        console.log("custom animation is done!" );
+      }
+    );
+  });
+
+  $.fn.opacify = function () {
+    this.animate(
+      { opacity: 1 },
+      300,
+      function() {
+        console.log('opacify plugin is done!');
+      }
+    )
+  }
+
+  $('#plugin').on('click', function() {
+    $('.card').opacify();
   })
 });
