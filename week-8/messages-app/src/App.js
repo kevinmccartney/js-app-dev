822a65f7bb8ce83573e77cb12a6e5c18a42a8e95
Kevin McCartney <hello@kevinmccartney.is>
2018-12-03 22:28:18 -0500

    Styling & adding delete button

--- week-8/messages-app/src/App.js
+++ week-8/messages-app/src/App.js
@@ -1,5 +1,6 @@
 // importing React runtime & the Component class
 import React, { Component } from 'react';
+import './App.css';
 
 // this is the single-component app that will allow you to manage messages
 class App extends Component {
@@ -19,6 +20,15 @@ class App extends Component {
         message: null,
       },
     };
+  }
+
+  handleDeleteMessage = id => {
+    const messagesWithoutDeletedMessage = this.state.messages.filter(message => message.id !== id);
+
+    this.setState({
+      ...this.state,
+      messages: messagesWithoutDeletedMessage,
+    });
   }
 
   // all of the following methods are arrow functions, which means the value of 'this' will be
@@ -46,7 +56,10 @@ class App extends Component {
   }
 
   // this method handles a submitting a new message
-  handleNewMessageSubmit = () => {
+  handleNewMessageSubmit = e => {
+    // we need to prevent the default event here - a form submission which would reload the page
+    e.preventDefault();
+
     // if there's no value in the new message input, don't create a new message
     if(this.state.newMessageValue) {
         this.setState({
@@ -71,7 +84,7 @@ class App extends Component {
     // don't submit the edited message if there's no value in the editing message input
     if(this.state.editingMessage.message) {
       // remove the previous message with the id of the message we are currently editing
-      const messagesNoEditingMessage = this.state.messages.filter((message) => message.id !== this.state.editingMessage.id);
+      const messagesNoEditingMessage = this.state.messages.filter(message => message.id !== this.state.editingMessage.id);
 
         this.setState({
           ...this.state,
@@ -101,60 +114,120 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {/*
-          Here we are using a controlled component, which means that we need to give it a value
-          explicitly & provide an onChange callback to update that value
-          https://reactjs.org/docs/forms.html#controlled-components
-        */}
-        <input
-          type="text"
-          value={this.state.newMessageValue}
-          onChange={e => this.handleNewMessageChange(e.target.value)}
-        />
-        <button
-          onClick={() => this.handleNewMessageSubmit()}
-        >
-          Submit Message
-        </button>
-        <section>
-          <ul>
-            {/* mapping over the messages in state */}
-            {this.state.messages.map(message => {
-                // if the editingMessage id is the same as the id of the message we are currently
-                // mapping over, render the editing interface
-                if(this.state.editingMessage.id === message.id) {
-                  return (
-                    <li>
-                      <input
-                        type="text"
-                        value={this.state.editingMessage.message}
-                        onChange={e => this.handleEditMessageChange(e.target.value)}
-                      />
-                      <button
-                        onClick={() => this.handleEditMessageSubmit()}
+      <div className="App pt-5 bg-light d-flex flex-column align-items-center">
+        <div>
+          <h1
+            className="mb-5"
+          >
+            Messages
+          </h1>
+          <form
+            className="form-inline mb-4"
+          >
+            <div
+              className="form-group"
+            >
+              {/*
+                Here we are using a controlled component, which means that we need to give it a value
+                explicitly & provide an onChange callback to update that value
+                https://reactjs.org/docs/forms.html#controlled-components
+              */}
+              <input
+                className="form-control"
+                type="text"
+                value={this.state.newMessageValue}
+                onChange={e => this.handleNewMessageChange(e.target.value)}
+              />
+              <button
+                className="btn btn-success ml-2"
+                onClick={e => this.handleNewMessageSubmit(e)}
+              >
+                Submit Message
+              </button>
+            </div>
+          </form>
+          <section>
+            <ul
+              className="list-group"
+            >
+              {/* mapping over the messages in state */}
+              {this.state.messages.map(message => {
+                  // if the editingMessage id is the same as the id of the message we are currently
+                  // mapping over, render the editing interface
+                  if(this.state.editingMessage.id === message.id) {
+                    return (
+                      <li
+                        className="list-group-item d-flex flex-column align-items-start"
+                      > 
+                        <form
+                          className="form-inline"
+                        >
+                           <div
+                              className="form-group d-flex flex-column align-items-start"
+                            >
+                              <input
+                                type="text"
+                                className="form-control mb-3"
+                                value={this.state.editingMessage.message}
+                                onChange={e => this.handleEditMessageChange(e.target.value)}
+                              />
+                              <div>
+                                <button
+                                  className="btn btn-primary mr-2"
+                                  onClick={() => this.handleEditMessageSubmit()}
+                                >
+                                  Submit
+                                </button>
+                                <button
+                                  className="btn btn-outline-danger"
+                                  onClick={() => this.toggleEditMode({ id: null, message: undefined })}
+                                >
+                                  Cancel
+                                </button>
+                              </div>
+                            </div>
+                        </form>
+                      </li>
+                    )
+                  }
+                  // otherwise render the display interface for the message
+                  else {
+                    return (
+                      <li
+                        className="list-group-item d-flex flex-column align-items-start"
                       >
-                        Submit
-                      </button>
-                    </li>
-                  )
-                }
-                // otherwise render the display interface for the message
-                else {
-                  return (
-                    <li>
-                      {message.message}
-                      <button
-                        // here we are passing the message object into the method call
-                        // because the method requires the entire object
-                        onClick={() => this.toggleEditMode(message)}
-                      >Edit</button>
-                    </li>
-                  )
-                }
-            })}
-          </ul>
-        </section>
+                        <div
+                          className="lead"
+                        >
+                          {message.message}
+                        </div>
+                        <div
+                          className="mt-3"
+                        >
+                          <button
+                            className="btn btn-primary mr-2"
+                            // here we are passing the message object into the method call
+                            // because the method requires the entire object
+                            onClick={() => this.toggleEditMode(message)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="btn btn-danger"
+                            // here we are passing the message object into the method call
+                            // because the method requires the entire object
+                            onClick={() => this.handleDeleteMessage(message.id)}
+                          >
+                            Edit
+                          </button>
+                        </div>
+                      </li>
+                    )
+                  }
+              })}
+            </ul>
+          </section>
+        </div>
       </div>
     );
   }
